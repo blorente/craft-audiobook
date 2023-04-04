0c9a78ff7ac734fba71f12fe7d678f0966f87e6b
Borja Lorente <blorente@apple.com>
2023-04-04 11:16:24 +0100

    Now with zipping

--- src/main.rs
+++ src/main.rs
@@ -31,6 +31,9 @@ struct Opt {
 
     #[structopt(long, short)]
     author: String,
+
+    #[structopt(long, short)]
+    zip: bool,
 }
 
 #[tokio::main]
@@ -40,6 +43,7 @@ async fn main() -> anyhow::Result<()> {
     let outdir = opt.outdir;
     let audiobook_name = opt.name;
     let audiobook_author = opt.author;
+    let zipit = opt.zip;
 
     create_dir_all(&outdir)?;
 
@@ -53,12 +57,16 @@ async fn main() -> anyhow::Result<()> {
             }
         }
     }
+    all_files.sort_by(|a, b| a.file_name().partial_cmp(&b.file_name()).unwrap());
 
     println!("== Converting {} files. ==", all_files.len());
     let ui = ConsoleUi::new()?;
 
-    let mut set: JoinSet<anyhow::Result<UiCommandId>> = JoinSet::new();
-    for file in all_files.iter() {
+    // let mut set: JoinSet<anyhow::Result<UiCommandId>> = JoinSet::new();
+    let mut set: JoinSet<anyhow::Result<()>> = JoinSet::new();
+    // TODO The UI can drop threads etween treturning the UI and stuff
+    // Maybe just dn't remove anything.
+    for (idx, file) in all_files.iter().enumerate() {
         let path = file.path();
         let basename = path
             .file_name()
@@ -80,24 +88,38 @@ async fn main() -> anyhow::Result<()> {
         set.spawn(async move {
             let pathstr = path.to_str().unwrap();
             let mp3out = format!("{}/{}.mp3", &outdir, &chapter_name);
-            let ui_id = ui.start_command(format!("Converting {} to {}", &chapter_name, &mp3out))?;
+            // let ui_id = ui.start_command(format!("Converting {} to {}", &chapter_name, &mp3out))?;
 
             let tts = TTS::new().expect("TODO");
             let aiff = tts.say(&pathstr, &outdir).await.expect("TODO").path;
-
+            let track = format!("{}", idx);
             let metadata = HashMap::from([
                 ("title", &chapter_name),
                 ("album", &audiobook_name),
                 ("author", &audiobook_author),
+                ("track", &track),
             ]);
             let convert_cmd = Converter::convert_aiff_to_mp3(&aiff, &mp3out, &metadata)?;
             convert_cmd.run().await?;
-            Ok(ui_id)
+            // Ok(ui_id)
+            Ok(())
         });
     }
 
     while let Some(res) = set.join_next().await {
-        let ui_id = res??;
+        // let ui_id = res??;
+        // ui.finish_command(ui_id)?;
+    }
+
+    if zipit {
+        let zip_out = format!("{}/{}.zip", &outdir, &audiobook_name);
+        let ui_id = ui.start_command(format!("Zipping into {}", &zip_out))?;
+        let args = vec!["zip", "-r", &zip_out, &outdir]
+            .into_iter()
+            .map(String::from)
+            .collect();
+        let zip_cmd = AsyncCommand::new(args)?;
+        zip_cmd.run().await?;
         ui.finish_command(ui_id)?;
     }
     println!("== All Done! ==");
